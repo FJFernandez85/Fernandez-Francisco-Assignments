091dc5c5d8a4467920ded6fa2816080d072de56e
FJFernandez85 <spurs956@gmail.com>
2015-04-16 21:31:42 -0500

    Prompted user for money spent on movies
    prompted user for the amount of money spent on movies monthly and I
    console.log the answer to check it

--- Fernandez_Francisco_Expressions_Assignment/js/week2.js
+++ Fernandez_Francisco_Expressions_Assignment/js/week2.js
@@ -4,10 +4,17 @@ Francisco Fernandez
 Expressions
  */
 
-// Movie Budget Calculator
+// Yearly Movie Budget Calculator
 
 //Ask the user for their favorite movie
 var name = prompt ("What is the name of your favorite movie?\nType in the movie title. ");
 
 //Im using the variable and saying wow and great movie
 alert("Wow! "+name+" is a great movie.");
+
+//Ask the user for the amount of money spent on movies each month
+var moneySpent = prompt ("How much money do you spend on movies monthly?");
+
+//console.log the answer to check it
+console.log("moneySpent");
+
