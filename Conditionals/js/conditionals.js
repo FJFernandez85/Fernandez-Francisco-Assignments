b591fb5a95494133f192c30443a210699cb82b01
FJFernandez85 <spurs956@gmail.com>
2015-04-18 15:13:09 -0500

    Conditional practice example
    decision making for the machine

---
+++ Conditionals/js/conditionals.js
@@ -0,0 +1,16 @@
+/*
+Francisco Fernandez
+SDI Section 01
+Conditionals
+4-18-2015
+*/
+
+//Conditional Logic
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
