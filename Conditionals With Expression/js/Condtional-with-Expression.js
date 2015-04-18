73f3ba6e9c69869736efd765d125feaf2ebdb6c9
FJFernandez85 <spurs956@gmail.com>
2015-04-18 16:29:09 -0500

    Logical operators
    using operators such as And = &&, < = less than, > = greater than

---
+++ Conditionals With Expression/js/Condtional-with-Expression.js
@@ -0,0 +1,19 @@
+/*
+ Francisco Fernandez
+ SDI Section 01
+ Conditional with expression
+ 4-18-2015
+ */
+
+//Conditional Logic - Logical Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+//if the price of the phone is less our budget And if our paycheck is over 300
+if(iPhonePrice < budget && paycheck > 300){
+    console.log("We can buy the phone!");
+}else{
+    console.log ("No phone for you!");
+}
