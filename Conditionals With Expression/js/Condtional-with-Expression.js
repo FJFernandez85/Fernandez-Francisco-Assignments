7c1acfb743380dfe5e44db97dfb6407fc665adbf
FJFernandez85 <spurs956@gmail.com>
2015-04-19 00:26:00 -0500

    Ternary Operator example 2
    another example on how to use Ternary form in place of conditional

--- Conditionals With Expression/js/Condtional-with-Expression.js
+++ Conditionals With Expression/js/Condtional-with-Expression.js
@@ -7,12 +7,14 @@
 
 //Conditional Logic - Ternary Operators
 
-var gpa = 48;
-
-//if the gpa is over the min 2.0, the student can graduate
-/*if(gpa > 2.0){
-    console.log("You can graduate!");
+var age = 6;
+var book;
+// if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+/*if(age <10){
+    book = "Green Eggs and Ham";
 }else{
-    console.log("GPA is too low!");
+    book = "The Time Machine";
 }*/
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+//Ternary Operator Example form
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
