b353c2afa91f603bb4b9c6654496d95ff58893b1
FJFernandez85 <spurs956@gmail.com>
2015-04-22 18:31:21 -0500

    created fruit array
    created fruit array

---
+++ Go To Training Week 3/js/gotoWeek3.js
@@ -0,0 +1,32 @@
+/*
+Francisco Fernandez
+4/22/2015
+GoTo Week #3
+Section 01
+ */
+
+//alert("Testing to see if connected.");
+
+//Create an array of fruit names and then sort it
+
+//Array - variable that holds multiple variables
+//Index # of an array always starts with 0!!
+
+var bowlofFruit = ["apple", "banana", "pear", "peach"];
+
+//See whole array
+console.log(bowlofFruit);
+
+//Console.log the pear
+console.log(bowlofFruit[2]);
+
+//How many items are in an array?
+//length property - access this by using dot syntax
+//Fancy term for use a period
+console.log(bowlofFruit.lengt);
+
+//Count the number of pears in our fruit bowl
+
+//Create a variable that will keep track of many pears there are.
+var totalNumPears = 0;
+
