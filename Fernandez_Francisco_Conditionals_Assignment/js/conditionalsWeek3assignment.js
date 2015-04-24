5a414ce242c058c1339dc2736384cb00e6ce1ee0
FJFernandez85 <spurs956@gmail.com>
2015-04-23 22:22:20 -0500

    else if statement and Ternary statement
    some changes on variables names and prompts changes. also completed my
    3 prompts

--- Fernandez_Francisco_Conditionals_Assignment/js/conditionalsWeek3assignment.js
+++ Fernandez_Francisco_Conditionals_Assignment/js/conditionalsWeek3assignment.js
@@ -7,18 +7,27 @@ Conditionals Assignment
 //How to defeat the zombie king?
 //alert user for help and prompt user for weapon selection
 
-var grenadeLauncher;
-var riFle;
-var piStol;
-var zombieKing;
-var timeLimit;
+var zombieKing = 500;
 
- grenadeLauncher = timeLimit-(zombieKing/30*1);
- riFle = timeLimit-(zombieKing/10*10);
- piStol = timeLimit-(zombieKing/4*5);
- zombieKing = 500;
- timeLimit = 120;
+//numberofTimes
 
-alert("We need your help to defeat the zombie king.");
+//alert("We need your help to defeat the zombie king.");
 
-prompt("Select the best weapon for the job.\n1.Grenade Launcher - dmg:20, sps:1\n2.Rifle - dmg:10, sps:10\n3.Pistol - dmg:4, sps:5\n(dmg - Damage per shot), (sps - Shots per Second)");
+//alert("This is the weapon selection .\n1.Grenade Launcher - dmg:20, sps:1\n2.Rifle - dmg:10, sps:10\n3.Pistol - dmg:4, sps:5\n(dmg - Damage per shot), (sps - Shots per Second)");
+
+var weaponChoice = prompt("Input the damage amount of your weapon of choice.\n1.Grenade Launcher : 30 dmg\n2.Rifle : 10 dmg\n3.Pistol : 4 dmg");
+console.log(weaponChoice);
+
+var shotsperSecond = prompt("Input the sps amount of your weapon of choice.\n1.Grenade Launcher : 1 sps\n2.Rifle : 10 sps\n3.Pistol : 5 sps");
+
+var numberofTimes = prompt("Input the number of times you have to use it to defeat the zombie king");
+console.log(numberofTimes);
+
+//else if Statement
+if(weaponChoice * shotsperSecond * numberofTimes > zombieKing ){
+    console.log("You defeated the zombie king!");
+}else{
+    console.log("You failed!");
+}
+//Ternary Statement
+(weaponChoice * shotsperSecond * numberofTimes > zombieKing) ? console.log("You defeated the zombie king!") : console.log("You failed!");
