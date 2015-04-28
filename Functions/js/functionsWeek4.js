af854d2001bf2fee5977416a312d8cb623c14ff7
FJFernandez85 <spurs956@gmail.com>
2015-04-27 22:16:15 -0500

    Functions Week 4
    functions_Basic Structure & Function Execution

---
+++ Functions/js/functionsWeek4.js
@@ -0,0 +1,25 @@
+/**
+Francisco Fernandez
+Functions
+4/27/2015
+*/
+
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+//Functions - Function Execution
+
+outptMsg();
+
+calcArea();
+
