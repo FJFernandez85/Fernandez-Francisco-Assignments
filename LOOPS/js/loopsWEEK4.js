0d58d0bb2301cd8f1bd7aec90426ae420d6710ae
FJFernandez85 <spurs956@gmail.com>
2015-04-29 00:24:48 -0500

    Loops- (while loop)
    created loops directory
    
    worked on while loop basics

---
+++ LOOPS/js/loopsWEEK4.js
@@ -0,0 +1,16 @@
+/*
+Francisco Fernandez
+Loops Week 4
+4-29-2015
+ */
+
+console.log('------ Loops ------');
+
+//while Loops
+
+var b = 50; //variable starting point,(setups the index)
+
+while (b > 0){ //replace if with "while", (checks the condition)
+    console.log(b + 'kegs on the wall' );
+    b--; //to increment it use ++ (increments or decrements the index)
+}
