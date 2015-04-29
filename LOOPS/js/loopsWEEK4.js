869df9f40fe7bacf509219a8b3379dea27d1b507
FJFernandez85 <spurs956@gmail.com>
2015-04-29 00:36:58 -0500

    Loops - (do - while - loop)
    created a do - while - loop
    
    “code always runs before condition is verified as true or false”

--- LOOPS/js/loopsWEEK4.js
+++ LOOPS/js/loopsWEEK4.js
@@ -14,3 +14,15 @@ while (b > 0){ //replace if with "while", (checks the condition)
     console.log(b + 'kegs on the wall' );
     b--; //to increment it use ++ (increments or decrements the index)
 }
+
+console.log('------ Do While Loops ------');
+
+
+var c = 10;
+
+do{
+    console.log(c + 'kegs okn the wall');
+    c--; // this code will always run before condition is verified as true or false
+}
+
+while(c > 0); //condition has to be true for code to run
