08374c8f52ca992d93ee086bd8aeeeaeca7815b7
FJFernandez85 <spurs956@gmail.com>
2015-04-30 01:38:32 -0500

    Prompts and alerts
    welcome alert to user, prompts for type and quantity of caps

--- Fernandez_Francisco_Functions_Assignment/js/functionsAssignment.js
+++ Fernandez_Francisco_Functions_Assignment/js/functionsAssignment.js
@@ -4,3 +4,22 @@ Francisco Fernandez
 Functions Assignment
 */
 
+//Team Caps
+
+//welcome alert to team caps
+alert ("Welcome to Team Caps!");
+console.log("Welcome to Team Caps");
+
+//prompt user to choose team
+var capTEAM = prompt("Choose a team from the list by typing the purchase cost on the side of the team!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)");
+console.log("Choose a team from the list by typing the purchase cost on the side of the team!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)");
+
+//prompt user to type number of caps to be purchased
+var numberofCAPS = prompt("Type number of caps that you want.");
+console.log("Type number of caps that you want.");
+
+function finalPRICE(){
+
+}
+
+
