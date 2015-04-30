fa036e1f30984132b1752e17db790f2d0e51bd20
FJFernandez85 <spurs956@gmail.com>
2015-04-30 07:22:48 -0500

    validated all prompts
    validated prompts using while loop

--- Fernandez_Francisco_Functions_Assignment/js/functionsAssignment.js
+++ Fernandez_Francisco_Functions_Assignment/js/functionsAssignment.js
@@ -11,15 +11,44 @@ alert ("Welcome to Team Caps!");
 console.log("Welcome to Team Caps");
 
 //prompt user to choose team
-var capTEAM = prompt("Choose a team from the list by typing the purchase cost on the side of the team!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)");
-console.log("Choose a team from the list by typing the purchase cost on the side of the team!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)");
+capTEAM = prompt("Choose a team from the list by typing purchase cost!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)\nex.(25, 30)");
+console.log("Choose a team from the list by typing purchase cost!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)\nex.(25, 30)");
 
-//prompt user to type number of caps to be purchased
-var numberofCAPS = prompt("Type number of caps that you want.");
-console.log("Type number of caps that you want.");
-
-function finalPRICE(){
-
+//validating capTeam prompt
+while(capTEAM==="" || isNaN(capTEAM)){
+    capTEAM = prompt("Please do not leave blank and only use numbers.\nChoose a team from the list by typing purchase cost!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)\nex.(25, 30)");
 }
 
+//prompt user to choose cap style
+capSTYLE = prompt("Choose the cap style you want by typing the cost of it!\n1.Fitted : $10\n2.Snapback : $5");
+console.log("Choose the cap style you want by typing the cost of it!\n1.Fitted : $10\n2.Snapback : $5");
+
+//validating capSTYLE prompt
+while(capSTYLE==="" || isNaN(capSTYLE)){
+    capSTYLE = prompt("Please do not leave blank and only use numbers.\nChoose the cap style you want by typing the cost of it!\n1.Fitted : $10\n2.Snapback : $5");
+}
+
+//prompt user to type number of caps to be purchased
+numberOFCAPS = prompt("Type number of caps that you want.");
+console.log("Type number of caps that you want.");
+
+//validating numberOFCAPS prompt
+while(numberOFCAPS==="" || isNaN(numberOFCAPS)){
+    numberOFCAPS = prompt("Please do not leave blank and only use numbers.\nType number of caps that you want.");
+}
+
+function checkoutCOST(numberOFCAPS){
+    var checkoutCOST = (numberOFCAPS) * (capTEAM + capSTYLE);
+    console.log("Final price is", checkoutCOST  );
+}
+checkoutCOST(1);
+checkoutCOST(2);
+checkoutCOST(3);
+checkoutCOST(4);
+checkoutCOST(5);
+checkoutCOST(6);
+checkoutCOST(7);
+checkoutCOST(8);
+checkoutCOST(9);
+checkoutCOST(10);
 
