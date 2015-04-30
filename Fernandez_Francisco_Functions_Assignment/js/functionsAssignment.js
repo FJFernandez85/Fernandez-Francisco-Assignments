501d6746582871efeec8621d6417bf36b26333b4
FJFernandez85 <spurs956@gmail.com>
2015-04-30 09:50:37 -0500

    complete function
    3 parameters, arguments and returned value

--- Fernandez_Francisco_Functions_Assignment/js/functionsAssignment.js
+++ Fernandez_Francisco_Functions_Assignment/js/functionsAssignment.js
@@ -11,7 +11,7 @@ alert ("Welcome to Team Caps!");
 console.log("Welcome to Team Caps");
 
 //prompt user to choose team
-capTEAM = prompt("Choose a team from the list by typing purchase cost!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)\nex.(25, 30)");
+var capTEAM = prompt("Choose a team from the list by typing purchase cost!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)\nex.(25, 30)");
 console.log("Choose a team from the list by typing purchase cost!\n1.SanAntonio Spurs ($25)\n2.Golden State Warriors ($30)\n3.Cleveland Cavaliers ($20)\n4.Chicago Bulls ($35)\nex.(25, 30)");
 
 //validating capTeam prompt
@@ -20,7 +20,7 @@ while(capTEAM==="" || isNaN(capTEAM)){
 }
 
 //prompt user to choose cap style
-capSTYLE = prompt("Choose the cap style you want by typing the cost of it!\n1.Fitted : $10\n2.Snapback : $5");
+var capSTYLE = prompt("Choose the cap style you want by typing the cost of it!\n1.Fitted : $10\n2.Snapback : $5");
 console.log("Choose the cap style you want by typing the cost of it!\n1.Fitted : $10\n2.Snapback : $5");
 
 //validating capSTYLE prompt
@@ -29,7 +29,7 @@ while(capSTYLE==="" || isNaN(capSTYLE)){
 }
 
 //prompt user to type number of caps to be purchased
-numberOFCAPS = prompt("Type number of caps that you want.");
+var numberOFCAPS = prompt("Type number of caps that you want.");
 console.log("Type number of caps that you want.");
 
 //validating numberOFCAPS prompt
@@ -37,18 +37,23 @@ while(numberOFCAPS==="" || isNaN(numberOFCAPS)){
     numberOFCAPS = prompt("Please do not leave blank and only use numbers.\nType number of caps that you want.");
 }
 
-function checkoutCOST(numberOFCAPS){
-    var checkoutCOST = (numberOFCAPS) * (capTEAM + capSTYLE);
-    console.log("Final price is", checkoutCOST  );
+
+//create function with 3 parameters, and returned value
+
+var returnedValue = checkoutCOST(capTEAM, capSTYLE, numberOFCAPS);
+
+console.log("Your Final charge is" + returnedValue);
+
+function checkoutCOST(team, style, quantity){
+    var finalCost = (team + style) * quantity;
+    console.log(finalCost);
+    return finalCost;
+
 }
-checkoutCOST(1);
-checkoutCOST(2);
-checkoutCOST(3);
-checkoutCOST(4);
-checkoutCOST(5);
-checkoutCOST(6);
-checkoutCOST(7);
-checkoutCOST(8);
-checkoutCOST(9);
-checkoutCOST(10);
+checkoutCOST(25,10,0);
+checkoutCOST(25,10,1);
+checkoutCOST(25,10,2);
+checkoutCOST(25,10,3);
+checkoutCOST(25,10,4);
+checkoutCOST(25,10,5);
 
